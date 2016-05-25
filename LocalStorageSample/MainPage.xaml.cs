5a093fdd06228140bd60f599e58c5221523ca551
Cristian González <prototipo_fauster@hotmail.com>
2016-05-25 01:50:11 -0500

    Add project files.

---
+++ LocalStorageSample/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using LocalStorageSample.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace LocalStorageSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            btnSave.Tapped += BtnSave_Tapped;
+        }
+
+        private void BtnSave_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            var service = new LocalStorageService();
+            var key = "personList";
+
+            List<Person> personList = new List<Person>();
+            personList.Add(new Person() { Name = "Cristian", Email = "cristian@microsoft.com" });
+            personList.Add(new Person() { Name = "Prannav", Email = "prannav@microsoft.com" });
+
+            // Here is where I save the list to the Application Settings
+            service.SaveList<Person>(personList, key);
+
+            // To retrieve the information stored in the App Settings:
+            var list = service.RetrieveList<Person>(key);
+        }
+    }
+}
---
+++ LocalStorageSample/Services/LocalStorageService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Newtonsoft.Json;
+
+namespace LocalStorageSample
+{
+    public class LocalStorageService
+    {
+        ApplicationDataContainer AppSettings = ApplicationData.Current.LocalSettings;
+
+        /// <summary>
+        /// Store information to the ApplicationData in the Local Settings.
+        /// You can use this method to retrieve the information.
+        /// </summary>
+        /// <typeparam name="ListType"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="key"></param>
+        public void SaveList<ListType>(List<ListType> list, string key)
+        {
+            // Serialize the list to a json
+            var json = JsonConvert.SerializeObject(list);
+
+            // Save the json to the app settings with the key we want
+            AppSettings.Values[key] = json;
+        }
+
+        /// <summary>
+        /// Retrieve the information from the Local Settings using the Application Data. 
+        /// </summary>
+        /// <typeparam name="ListType"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public List<ListType> RetrieveList<ListType>(string key)
+        {
+            // Search if there is the Setting with the desired key
+            if (AppSettings.Values.ContainsKey(key))
+            {
+                // Retrive the value from the App Settings.
+                var value = AppSettings.Values[key].ToString();
+
+                // Deserialize the Object from the List. 
+                var list = JsonConvert.DeserializeObject<List<ListType>>(value);
+
+                // And we return the list.
+                return list;
+            }
+
+            // If there is no value stored, we return null.
+            return null;
+        }
+    }
+}
